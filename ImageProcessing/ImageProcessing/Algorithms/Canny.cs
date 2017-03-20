13782f9bd355110d11d602ff9a46267401b7c053
dobrybajer <kamil.zakk@gmail.com>
2017-03-21 00:50:24 +0100

    Empty classes

---
+++ ImageProcessing/ImageProcessing/Algorithms/Canny.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageProcessing.Interfaces;
+
+namespace ImageProcessing.Algorithms
+{
+    internal class Canny : IAlgorithm
+    {
+        public void ProcessImage(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ImageProcessing/ImageProcessing/Data/FileManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessing.Data
+{
+    internal static class FileManager
+    {
+        public static void ReadInput()
+        {
+            var input = ConfigurationManager.AppSettings["InputFolder"];
+        }
+
+        public static void WriteOutput()
+        {
+            var output = ConfigurationManager.AppSettings["OutputFolder"];
+        }
+    }
+}
