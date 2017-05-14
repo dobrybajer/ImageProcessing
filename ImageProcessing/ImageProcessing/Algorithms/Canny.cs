074bfaa0e64ffb64ec381c31c81df5fdfb61af3b
dobrybajer <kamil.zakk@gmail.com>
2017-05-15 00:56:20 +0200

    Podział pracy

--- ImageProcessing/ImageProcessing/Algorithms/Canny.cs
+++ ImageProcessing/ImageProcessing/Algorithms/Canny.cs
@@ -11,7 +11,7 @@
         public override Bitmap ProcessImage(Bitmap image)
         {
             var newImage = new Bitmap(image);
-            
+            // TODO by Kamil
             return newImage;
         }
     }
